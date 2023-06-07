b3673fe73dfc9329343a13894cddccd8c366b1f6
Max Häggman <max.haggman@edu.cmeducations.se>
2023-06-07 13:10:42 +0200

    auth and middleware

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,26 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import RootLayout from "../components/root-layout";
 
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+
 export default function App({ Component, pageProps }: AppProps) {
+
+  const [supabaseClient] = useState(() => createPagesBrowserClient())
+
+
   return (
     <>
-      <RootLayout>
-        <Component {...pageProps} />
-      </RootLayout>
-      <div id="root" />
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
+        <div id="root" />
+      </SessionContextProvider>
     </>
   );
 }
--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,5 +1,32 @@
-import Heading from "@components/heading";
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
-export default function Login() {
-  return <Heading>Login</Heading>;
+const LoginPage = () => {
+  const supabaseClient = useSupabaseClient()
+  const user = useUser() 
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (user) {
+      router.push('/')
+    }
+  }, [user, router])
+
+  return (
+    <>
+      <Auth
+        redirectTo="http://localhost:3000/"
+        appearance={{ theme: ThemeSupa }}
+        supabaseClient={supabaseClient}
+        providers={[]}
+        socialLayout="horizontal"
+      />
+    </>
+  )
 }
+
+export default LoginPage
