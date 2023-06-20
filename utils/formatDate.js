d52df60c504ad366832de0dbb484f39d2530c0ee
Max Häggman <max.haggman@edu.cmeducations.se>
2023-06-20 15:25:37 +0200

    remove display of seconds

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -3,7 +3,7 @@ import { parseISO, parseJSON, format } from "date-fns";
 export const formatDate = (date) => {
     const dateString = date;
     const newDate = parseISO(dateString);
-    const formattedDate = format(newDate, 'yyyy-MM-dd HH:mm:ss');
+    const formattedDate = format(newDate, 'yyyy-MM-dd HH:mm');
   
     
     return { formattedDate }
