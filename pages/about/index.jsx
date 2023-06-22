ca573e27a9082506645eb02fc53626f82de36822
Max Häggman <max.haggman@edu.cmeducations.se>
2023-06-22 10:59:03 +0200

    add empty div

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,5 +1,10 @@
 import Heading from "@components/heading";
 
 export default function About() {
-  return <Heading>About</Heading>;
+  return (
+    <>
+      <Heading>About</Heading>
+      <div>About page is empty</div>
+    </>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
 import Heading from "@components/heading";
 
 export default function Home() {
-  return <Heading>Home</Heading>;
+  return (
+    <>
+      <Heading>Home</Heading>
+      <div>Home page is empty</div>
+    </>
+  )
 }
